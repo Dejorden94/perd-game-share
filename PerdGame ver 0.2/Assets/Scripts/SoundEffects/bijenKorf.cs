7e90c28c75d7a1ae9926af3b26357a672868b962
Dejorden94 <d.moerman@hotmail.nl>
2020-05-06 15:49:44 +0200

    Game update
    - sound effects toegevoegd
    - ridder positie verbeterd.

---
+++ PerdGame ver 0.2/Assets/Scripts/SoundEffects/bijenKorf.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bijenKorf : MonoBehaviour
+{
+
+public AudioSource bijen;
+
+public AudioSource ouch;
+
+public AudioSource hit;
+
+void OnTriggerEnter(Collider other) 
+   {
+       bijen.Play();
+       ouch.Play();
+       hit.Play();
+   }
+}
