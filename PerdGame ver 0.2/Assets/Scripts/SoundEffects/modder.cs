33334c55f83c98abe8804ec3392cda8d4164fe64
Dejorden94 <d.moerman@hotmail.nl>
2020-05-08 16:59:13 +0200

    Game update
    - Pijl UI verandert.
    - water en modder voorzien van geluid
    - punten voor lantaarns op het einde.

---
+++ PerdGame ver 0.2/Assets/Scripts/SoundEffects/modder.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class modder : MonoBehaviour
+{
+   public AudioSource modderPad;
+
+void OnTriggerEnter(Collider other) 
+   {
+       modderPad.Play();
+   }
+}
---
+++ PerdGame ver 0.2/Assets/Scripts/SoundEffects/water.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class water : MonoBehaviour
+{
+   public AudioSource splash;
+
+void OnTriggerEnter(Collider other) 
+   {
+       splash.Play();
+   }
+}
